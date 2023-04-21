0f4ba775720a2819d4307f9e4e89c019d72b9f6e
Madjar Ivan <evanmadjar@gmail.com>
2023-04-21 18:59:05 +0300

    I added models to 'posts' app

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from common.mixins.models import BaseModel
+from common.fields import OptionalCharField
+from users.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='posts', blank=True)
+    text = models.CharField(max_length=255)
+    name = OptionalCharField()
+    avatar = models.URLField(max_length=200, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.text
+
+    class Meta:
+        ordering = ('-date',)
+
+
+class Comment(BaseModel):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='user_comments', blank=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,
+        related_name='post_comments', blank=True)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.text} - {self.user.name}'
+
+    class Meta:
+        ordering = ('-date',)
