d07990f0abee7e82438a687582cb52c31b4b8502
Madjar Ivan <evanmadjar@gmail.com>
2023-04-25 16:29:32 +0300

    I created routes to users app

---
+++ backend/apps/users/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import (
+    UserView,
+    GetAuthUserView,
+)
+
+
+urlpatterns = [
+    path('users', UserView.as_view()),
+    path('auth', GetAuthUserView.as_view()),
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,7 @@ API_PREFIX = 'api/v1'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{API_PREFIX}/', include('posts.api.urls')),
+    path(f'{API_PREFIX}/', include('users.api.urls')),
 ]
 
 urlpatterns += [
